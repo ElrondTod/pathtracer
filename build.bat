c3871e46e7aa0dfa61bedebea4e9e90905f1d4d1
ElrondTod <elrondtod@gmail.com>
2020-03-16 03:56:58 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cls
-
-g++ -Wall -Wextra -I header src\vector.cpp src\material.cpp src\object.cpp src\camera.cpp src\math.cpp src\writer.cpp src\triangle.cpp src\renderer.cpp src\scene.cpp src\main.cpp
